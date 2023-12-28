ac559aa92b6c46e89c2c1aa101e8535261ffa3a5
kkathereliu <katherineliu327kl@gmail.com>
2023-12-28 17:51:07 -0500

    game board is proportionate to screen

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let wordsComplete = 0;
 let speed = 35; // change with user input from the website after
 // in the future maybe grab this from an API (randwords)
 let wordList = ['worth', 'even', 'tolerant', 'button', 'orange', 'dance', 'person', 'shout', 'house', 'mosque', 'snarl', 'snub', 'studio', 'destruction', 'island', 'revive', 'time', 'carbon', 'try', 'salon', 'miner', 'misplace', 'current', 'truth', 'guideline', 'conglomerate', 'employ', 'progress', 'tap', 'place', 'crisis', 'pour', 'silver', 'literature', 'seem', 'appeal', 'tray', 'location', 'stretch', 'list', 'differ', 'bag', 'disorder', 'speed', 'relinquish', 'pump', 'peak', 'offer', 'soar', 'register']; 
+const board = document.getElementById('board');
+let boardHeight = 0.8*visualViewport.height;
+let boardWidth = 0.75*visualViewport.width;
 
 window.onload = () => {
     document.getElementById('input').addEventListener('keypress', 
@@ -23,10 +26,13 @@ for (let i = 0; i < 1000; i++) {
 
     setTimeout(() => {
         addWord(word);
-        // slide(); commented out to fix styling
+     //   slide(); 
     }, i*1000); // word is added every second, can maybe change in game with user input of difficulty/increase as time passes
     // requires i*1000 so that each element is 1000 APART (first one is delayed 1000, second one 2000 etc.)
     // otherwise it would just give 1000 to all of them and they'd execute together still
+    console.log(boardWidth + '   ' +boardHeight);
+    console.log(parseInt(board.style.width) + '   ' +parseInt(board.style.height));
+    console.log("\n");
 }
 
 function enteredWord(word) {
@@ -46,14 +52,13 @@ function enteredWord(word) {
 
 function addWord(word) {
     if (document.getElementById(word) === null) {
-        const board = document.getElementById('board');
         // the below is better than using innerHTML
         let div = document.createElement('div'); 
         div.setAttribute('id', word);
         div.style.position = 'absolute';
         div.style.color = 'white';
-        div.style.left = Math.floor(Math.random()*300 + 50) + 'px';
-        div.style.top = Math.floor(Math.random()*45) + 'px'; // note that board margin is 50 so words generated off the board
+        div.style.left = Math.floor(Math.random()*(boardWidth-100)) + 'px';
+        div.style.top = Math.floor(Math.random()*-20) + 'px'; // note that board margin is 50 so words generated off the board
         board.appendChild(div);
         div.innerText = word;
         wordsDisplayed.push(word);
@@ -65,13 +70,11 @@ function slide() {
     wordsDisplayed.forEach(w => {
         const word = document.getElementById(w);
         let yCoor = parseInt(word.style.top);
-        const root = document.querySelector(':root');
-        const boardHeight = parseInt(getComputedStyle(root).getPropertyValue('--gameHeight'));
         
         if (yCoor < boardHeight) {
             yCoor ++; // could be fun to change the speed of different words so its not the same 
             word.style.top = yCoor + 'px';
-            if (yCoor > (0.8 * boardHeight)) {
+            if (yCoor > (boardHeight - 100)) {
                 word.style.color = 'red';
             } 
         } else {
@@ -87,6 +90,3 @@ function removeWord(word) {
     document.getElementById(word).remove();
     wordsDisplayed = wordsDisplayed.filter((w) => {return w !== word;});
 }
-
-
-
