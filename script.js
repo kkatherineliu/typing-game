5384f66ee081629baed87c008d5b26181ab86cc5
kkathereliu <katherineliu327kl@gmail.com>
2023-12-28 14:33:38 -0500

    understanding the code better and trying to fix sliding

--- script.js
+++ script.js
@@ -9,59 +9,64 @@ window.onload = function(){
         if (e.key === "Enter") {
             //e.preventDefault();
             enteredWord(this.value);
-            console.log(this.value + ' was entered');
             this.value = ''; 
         }
     });
 }
 
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 1000; i++) { 
+    // maybe change to a while loop basically for game state (while its still going on?)
+    // currently the loop basically will add words 1000 times total
     let word;
     do {
-        wordList[Math.floor(Math.random()*wordList.length)];
+        word = wordList[Math.floor(Math.random()*wordList.length)];
     } while (wordsDisplayed.has(word)); // avoid duplicate values bc dealing with sets
+    // basically rn two of the same word will show up on the screen and then typing only deletes one of them hmm
 
     setTimeout(function timer(){
         addWord(word);
         slide();
-    }, i*1000); // do it every second, can maybe change in game
+    }, i*1000); // word is added every second, can maybe change in game with user input of difficulty/increase as time passes
+    // requires i*1000 so that each element is 1000 APART (first one is delayed 1000, second one 2000 etc.)
+    // otherwise it would just give 1000 to all of them and they'd execute together still
+    // only thing is i don't understand how the slide part works, going wayy too slow
+    
 }
-let interval = setInterval(slide(),30);
 
 function enteredWord(word) {
     if (wordsDisplayed.has(word)) {
-        console.log(this.value + ' was entered CORRECTLY');
         removeWord(word);
         wordsComplete ++;
-        document.getElementById('score').innerText = wordsComplete;
+        document.getElementById('score').innerText = `Words Answered: ${wordsComplete}`;
     }
 }
 
 function addWord(word) {
-    let board = document.getElementById('board');
-    // the below is better than using innerHTML
-    let div = document.createElement('div'); 
-    div.setAttribute('id', word);
-    div.style.position = 'absolute';
-    div.style.color = 'white';
-    div.style.left = Math.floor(Math.random()*300 + 50) + 'px';
-    div.style.top = Math.floor(Math.random()*50 + 50) + 'px';
-    board.appendChild(div);
-    div.innerText = word;
-    wordsDisplayed.add(word);
+    if (document.getElementById(word) === null) { // extra security to prevent duplicates
+        let board = document.getElementById('board');
+        // the below is better than using innerHTML
+        let div = document.createElement('div'); 
+        div.setAttribute('id', word);
+        div.style.position = 'absolute';
+        div.style.color = 'white';
+        div.style.left = Math.floor(Math.random()*300 + 50) + 'px';
+        div.style.top = Math.floor(Math.random()*50 + 50) + 'px';
+        board.appendChild(div);
+        div.innerText = word;
+        wordsDisplayed.add(word);
+    }
 }
 
+let interval = setInterval(slide(),30); //idt this is doing anything rn
 function slide() {
     wordsDisplayed.forEach(function(w) {
         let word = document.getElementById(w);
-        console.log(word.style.top);
         let yCoor = parseInt(word.style.top);
         // let root = document.querySelector(':root');
         if (yCoor < 500) {
-            //  getComputedStyle(root).getPropertyValue('--gameHeight')
+            // getComputedStyle(root).getPropertyValue('--gameHeight')
             yCoor ++;
             word.style.top = yCoor + 'px';
-            console.log(w + '     ' + yCoor + '    ' + word.style.top);
         } else {
             clearInterval(interval);
             removeWord(w);
@@ -71,8 +76,8 @@ function slide() {
     });
 }
 
-function removeWord(w) {
-    document.getElementById(w).remove(); //not the same as demo, hope this works, not sure though
+function removeWord(word) {
+    document.getElementById(word).remove(); //not the same as demo, hope this works, not sure though
     wordsDisplayed.delete(word);
 }
 
